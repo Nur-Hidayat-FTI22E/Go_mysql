b6a4de7c0a4f5cffc086efa0625f50b316dc24a3
Log-Hori'xins <133627973+Nur-Hidayat-FTI22E@users.noreply.github.com>
2023-09-03 20:52:48 +0000

    connection

---
+++ database_test.go
@@ -0,0 +1,21 @@
+package gomysql
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Test(t *testing.T) {
+
+}
+
+func TestOpenconnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:nra1223004@tcp(localhost:3306)/")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Nur-Hidayat-FTI22E/Go_mysql
+
+go 1.21.0
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
